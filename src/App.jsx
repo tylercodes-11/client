d440983d377f7270add23c5701ff96f788f789f5
Ty Reid <trawx11@gmail.com>
2023-03-05 15:08:58 -0500

    added context to the main.jsx - sideb icons fix

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,13 @@ const  App = () => {
         <Navbar />
 
         <Routes>
+          
           <Route path="/" element={<Home />}
         />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/create-campaign" element={<CreateCampaign />} />
+        <Route path="/campaign-details/:id" element={<CampaignDetails />} />
+
         </Routes>
 
       </div>
